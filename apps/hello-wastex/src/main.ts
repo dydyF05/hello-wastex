f3f58bfea260a0ad82bd9e4d8bd9e59f2ef74a66
dydyf05 <sobriicivis@protonmail.com>
2024-05-16 10:54:09 +0200

    add pinia

--- apps/hello-wastex/src/main.ts
+++ apps/hello-wastex/src/main.ts
@@ -1,8 +1,10 @@
-import './styles.scss';
-
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './app/App.vue';
+import './styles.scss';
 
+const pinia = createPinia();
 const app = createApp(App);
+app.use(pinia);
 
 app.mount('#root');
